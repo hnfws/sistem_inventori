b7934db3322165bc39934dc0d710f558f4cb9b85
Nisaalfiatur <99831962+Nisaalfiatur@users.noreply.github.com>
2025-12-23 01:49:52 +0700

    Feature/nisa alfiatur rohmah 1324092 (#5)
    * Add files via upload
    
    menambhakan file pada git
    
    * Update Laporan.java
    
    menghapus semua baris pada laporan.java karena fitur tidak diperlukan
    
    * Update Karyawan.java
    
    menghapus code pada karyawan karena tidak diperlukan
    
    * Update Member.java
    
    menghapus kode pada member.java karena tidak diperlukan
    
    * Update Transaksi.java
    
    Menghapus  code pada transaksi.java karena tidak diperlukan dalam projek
    
    * Update User.java
    
    Menghapus code pada user.java karena tidak diperlukan dalam projek
    
    * Update uh.java
    
    menghapus code pada file uh.java karena tidak diperlukan dalam projek
    
    * Create anggota2.java
    
    menambahkan NIM dan nama
    
    * Update Barang.java
    
    menambahkan fitur kategori, barang terakhir masuk, dan barang terakhir keluar pada array list
    
    * Create StokHabisException.java
    
    membuat kelaas baru dengan nama StokHabisException.java
    
    * Create KategoriBarang.java
    
    membuat kelas kategoribarang.java yang terintegrasi dengan kelas barang.java
    
    * Update KategoriBarang.java
    
    menambahkan kode pada kategoribarang.java

---
+++ anggota2.java
@@ -0,0 +1,11 @@
+public class AnggotaSimple {
+    public static void main(String[] args) {
+        String nama = "Nisa Alfiatur Rohmah";
+        String nim = "13240092";
+        String jurusan = "Sistem Informasi Industri Otomotif";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Jurusan: " + jurusan);
+    }
+}
