9df8b60755dd30614f8d5f6c74ef986cc8912c48
Fatimah Nuraini <ainifah16@gmail.com>
2025-12-23 22:23:02 +0800

    Refactor BarangController to include new methods

--- BarangController.java/BarangController.java
+++ BarangController.java/BarangController.java
@@ -1,72 +1,35 @@
 package tokoOnline;
 
 public class BarangController {
-
     private Barang barang;
+    private int totalQtyMasuk = 0;
+    private int totalQtyKeluar = 0;
 
-    public BarangController() {
-        barang = new Barang();
+    public BarangController(Barang barang) {
+        this.barang = barang;
     }
 
-    // ===== GET DATA =====
-    public int getJumlahBarang() {
-        return barang.getJmlBarang();
+    public void prosesTambahProduk(String nama, String kategori, int stok, int harga) {
+        barang.tambahDataBaru(nama, kategori, stok, harga);
     }
 
-    public String getNamaBarang(int id) {
-        return barang.getNamaBarang(id);
+    // Menambahkan throws StokHabisException agar bisa ditangkap oleh GUI
+    public void prosesBarangMasuk(int modelRow, int qty) throws StokHabisException {
+        barang.editStok(modelRow, barang.getStok(modelRow) + qty);
+        barang.setWaktuMasuk(modelRow, java.time.LocalDateTime.now()
+                .format(java.time.format.DateTimeFormatter.ofPattern("HH:mm dd-MM-yyyy")));
+        totalQtyMasuk += qty;
     }
 
-    public int getStok(int id) {
-        return barang.getStok(id);
+    public void prosesBarangKeluar(int modelRow, int qty) throws StokHabisException {
+        barang.editStok(modelRow, barang.getStok(modelRow) - qty);
+        barang.setWaktuKeluar(modelRow, java.time.LocalDateTime.now()
+                .format(java.time.format.DateTimeFormatter.ofPattern("HH:mm dd-MM-yyyy")));
+        totalQtyKeluar += qty;
     }
 
-    public int getHarga(int id) {
-        return barang.getHarga(id);
-    }
-
-    public String getWaktuMasuk(int id) {
-        return barang.getWaktuMasuk(id);
-    }
-
-    public String getWaktuKeluar(int id) {
-        return barang.getWaktuKeluar(id);
-    }
-
-    // ===== TAMBAH BARANG =====
-    public void tambahBarang(String nama, int stok, int harga) {
-        barang.setNamaBarang(nama);
-        barang.setStok(stok);
-        barang.setHarga(harga);
-    }
-
-    // ===== BARANG MASUK =====
-    public boolean barangMasuk(int id, int jumlah, String waktu) {
-        if (jumlah <= 0) return false;
-
-        int stokBaru = barang.getStok(id) + jumlah;
-        barang.editStok(id, stokBaru);
-        barang.setWaktuMasuk(id, waktu);
-        return true;
-    }
-
-    // ===== BARANG KELUAR =====
-    public boolean barangKeluar(int id, int jumlah, String waktu) {
-        if (jumlah <= 0) return false;
-
-        int stokSekarang = barang.getStok(id);
-        if (stokSekarang < jumlah) return false;
-
-        barang.editStok(id, stokSekarang - jumlah);
-        barang.setWaktuKeluar(id, waktu);
-        return true;
-    }
-
-    // ===== STATUS =====
-    public String getStatus(int id) {
-        int stok = barang.getStok(id);
-        if (stok == 0) return "HABIS";
-        if (stok < 5) return "MENIPIS";
-        return "AMAN";
-    }
+    public int getTotalJenisProduk() { return barang.getJmlBarang(); }
+    public int getTotalQtyMasuk() { return totalQtyMasuk; }
+    public int getTotalQtyKeluar() { return totalQtyKeluar; }
+    public Barang getModel() { return barang; }
 }
